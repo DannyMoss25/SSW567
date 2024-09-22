a9cb3cf2c099097185ee61223f0cc6d5100b0eb2
DannyMoss25 <144484814+DannyMoss25@users.noreply.github.com>
2024-09-22 18:38:32 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,71 @@
+import unittest
+
+def classify_triangle(a,b,c):
+    endMessage = ""
+
+    if (str(type(a)) == "<class 'str'>"):
+        return "STRING VALUE"
+
+
+    if(a == 0 or b == 0 or c == 0):
+        return "ZERO VALUE"
+
+    if (a < 0 or b < 0 or c < 0):
+        return "ONLY POSITIVE NUMBERS ACCEPTED"
+
+    if(a == b and b == c):
+        endMessage += "equilateral"
+    elif(a == b or b == c or a == c):
+        endMessage += "isosceles"
+    else:
+        endMessage += "scalene"
+
+    c = round(c * c, 3)
+    a = round(a * a, 3)
+    b = round(b * b, 3)
+
+    if(a + b == c  or c  + b == a  or a  + c  == b ):
+        endMessage += ", Right Triange"
+
+    return endMessage
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_triangle(self):
+        self.assertEqual("equilateral", classify_triangle(3, 3, 3))
+        self.assertEqual("equilateral", classify_triangle(5.5, 5.5, 5.5))
+
+        self.assertEqual("isosceles", classify_triangle(3, 3, 5))
+        self.assertEqual("isosceles", classify_triangle(3, 5, 3))
+        self.assertEqual("isosceles", classify_triangle(5, 3, 3))
+
+        self.assertEqual("scalene", classify_triangle(5, 3, 8))
+        self.assertEqual("scalene", classify_triangle(3, 4, 9))
+        self.assertEqual("scalene", classify_triangle(5, 3, 8))
+
+        self.assertEqual("scalene, Right Triange", classify_triangle(3,4,5))
+        self.assertEqual("scalene, Right Triange", classify_triangle(3, 5, 4))
+        self.assertEqual("scalene, Right Triange", classify_triangle(4, 5, 3))
+        self.assertEqual("scalene, Right Triange", classify_triangle(8, 15, 17))
+        self.assertEqual("scalene, Right Triange", classify_triangle(9, 41, 40))
+
+        self.assertEqual("isosceles, Right Triange", classify_triangle(2, 2, (2 ** 0.5) * 2))
+        self.assertEqual("isosceles, Right Triange", classify_triangle(8, 8, (2 ** 0.5) * 8))
+        self.assertEqual(str("STRING VALUE"), str(classify_triangle("B", 2, 8)))
+
+        self.assertEqual("ZERO VALUE", classify_triangle(0, 4, 5))
+        self.assertEqual("ZERO VALUE", classify_triangle(3, 0, 5))
+        self.assertEqual("ZERO VALUE", classify_triangle(3, 4, 0))
+
+        self.assertEqual("ONLY POSITIVE NUMBERS ACCEPTED", classify_triangle(-3, 4, 5))
+        self.assertEqual("ONLY POSITIVE NUMBERS ACCEPTED", classify_triangle(3, -4, 5))
+        self.assertEqual("ONLY POSITIVE NUMBERS ACCEPTED", classify_triangle(3, 4, -5))
+
+        self.assertNotEqual("isosceles, Right Triange", classify_triangle(2, 2, 8))
+        self.assertNotEqual("scalene", classify_triangle(3, 3, 1))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
